b34dc763d69cc2f46bf1dc53b8cbeef3cf138099
Steven Love <lovestevend@gmail.com>
2015-02-17 00:15:30 -0500

    failing test cases for basic board(task 655)

--- main.cc
+++ main.cc
@@ -1,30 +1,30 @@
-#include <iostream>
+#include <stdio.h>
 
-// int main(){
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from gtest_main.cc\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
 
 
-// };
-
-int preset(){
-	return 5;
-};
-
-// class Board{
+class Board{
 	
-// 	public:
-// 		int pips[25]={0}; //0 unused
-// 		int homes[2]={0}; //white is 0, black is 1
-// 		int bars[2]={0};  //white is 0, black is 1
+	public:
+		int pips[25]={0}; //0 unused
+		int homes[2]={0}; //white is 0, black is 1
+		int bars[2]={0};  //white is 0, black is 1
 
-// 		void initialize(){
-// 			pips[24] =  2; // 2 white stones on pip 24
-// 			pips[19] = -5; // 5 black stones on pip 19
-// 			pips[17] = -3; // ...
-// 			pips[13] =  5;
-// 			pips[12] = -5;
-// 			pips[8]  = -3;
-// 			pips[6]  =  5;
-// 			pips[1]  = -2;
-// 		};
-// };
+		void initialize(){
+			pips[24] =  2; // 2 white stones on pip 24
+			pips[19] = -5; // 5 black stones on pip 19
+			pips[17] = -3; // ...
+			pips[13] =  5;
+			pips[12] = -5;
+			pips[8]  = -3;
+			pips[6]  =  5;
+			pips[1]  = -2;
+		};
+};
--- tests/inittest.cc
+++ tests/inittest.cc
@@ -2,9 +2,36 @@
 #include "../main.cc"
 #include "gtest/gtest.h"
 
-TEST(preset,Negative){
+TEST(Initialization,Pip24){
 
-	ASSERT_EQ(5,5) << "preset was not 5";
-	ASSERT_EQ(5,6) << "should not work";
-	
+	// ASSERT_EQ(preset(),5) << "preset was not 5";
+	// EXPECT_EQ(4,preset()) << "should not work";
+
+	Board *b = new Board();
+
+	EXPECT_EQ(b->pips[24],2) <<  "we want 2 white stones on pip 24";
+
+	b->initialize();
+
+	ASSERT_EQ(b->pips[24],3) <<  "we better have 2 white stones on pip 24";
+
+}
+
+TEST(Initialization,WhiteCount){
+	Board *b = new Board();
+	b->initialize();
+
+	// Count up the white stones
+	int count = 0;
+	int pipval;
+	for(int i = 1; i < 25; ++i){
+		pipval = b->pips[i];
+		if(pipval > 0){ // white stones are here
+			count += pipval;
+		}
+	}
+	// now count = # white stones on the board
+
+	ASSERT_EQ(count,15) <<  "we want 15 white stones on the board";
+
 }
