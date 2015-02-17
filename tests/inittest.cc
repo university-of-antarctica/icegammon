5bcb8fcaad424589523a4051700f67fde42b1beb
Steven Love <lovestevend@gmail.com>
2015-02-17 13:36:36 -0500

    refactored turns (task 706)

--- tests/inittest.cc
+++ tests/inittest.cc
@@ -1,4 +1,5 @@
-//#include <stdio.h>
+//#include <stdio.h> //
+#include <stdlib.h> //rand, srand
 #include "testmain.cc"
 #include "gtest/gtest.h"
 
@@ -61,4 +62,27 @@ TEST(Turns, First4TurnsAlternate){
 	EXPECT_EQ(first,third) << "first and third turns should be the same color";
 	EXPECT_EQ(second,fourth) << "second and fourth turns should be the same color";
 	EXPECT_NE(first,second) << "first and second turns shouldn't be the same color";
+}
+
+TEST(Turns, EvenOddRandomized){
+	Game *g = new Game();
+	Color first = g->turn;
+	g->passTurn();
+	Color second = g->turn;
+
+	int n = rand() % 1000; //in the range between 0 and 999
+	for(int i = 0; i < n; ++i){
+		g->passTurn();
+	}
+
+	//if n is odd, then the color should be same as the first color
+	//if n is even, then the color should be the same as the second color
+	if(n%2==1){//n is odd
+		EXPECT_EQ(first,g->turn)<< "all odd numbered turns should be the same color";
+	}
+	else{//n is even
+		EXPECT_EQ(second,g->turn) << "all even numbered turns should be the same color";
+	}
+	EXPECT_NE(first,second) << "first and second turns shouldn't be the same color";
+
 }
