bd15d6f183233bc7b6f1c0dd40d43dc5d17d12c3
Steven Love <lovestevend@gmail.com>
2015-03-17 11:03:40 -0400

    added CLEANCODE comments to Controller.h

--- include/Controller.h
+++ include/Controller.h
@@ -21,22 +21,22 @@
 
   public:
   Controller(Game*);
-  std::pair<DieFace,DieFace> getFirstTurnRolls(bool);
+  std::pair<DieFace,DieFace> getFirstTurnRolls(bool);//The dice maintain state, so we should just be able to leave the dice instead of returning these values
   void queryPlayerForRoll(bool);
   DieFace getFirstDieRoll();
-  bool inputValidationForDiceRollPrompt(std::string*);
+  bool inputValidationForDiceRollPrompt(std::string*);//CLEANCODE: javabean predicate rule
   
   //implement these! (and test)
   Turn* queryPlayerForMoveObject(bool); //CLEANCODE: misnamed, this prompts a player for a turn object, not a move object. good opportunity to make an actual promptPlayerForMoveObject method
   bool inputValidationForMoveObject(std::string); //CLEANCODE: doesn't follow javabean standard for predicates (isValid might be better)
-  int getNumMoves(); 
+  int getNumMoves(); //will need to be more complicated since we have to enumerate all turns to find the one with the most moves, right?
   bool getTurn(bool); //CLEANCODE: this doesn't follow javabean standard for get methods, it is a pun
-  Move* parseMove(std::string);
+  Move* parseMove(std::string); //CLEANCODE: we're parsing a move string into a move object, so, parseMoveString(std::string)?
   void promptAndPerformRoll(bool test); //CLEANCODE: be consistent with our language.  everything should be 'query' or 'prompt' (my preference) for methods that block waiting for user input
 
   private:
   Game* game;
-  void getUserInputLine(std::string*);
+  void getUserInputLine(std::string*); //CLEANCODE: get method returns void?
   std::vector<std::string> splitByWhiteSpace(std::string);
   int numDigits(int);
   };
