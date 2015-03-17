758934acd5c271a4593f53cd9921dde9650f5a65
Steven Love <lovestevend@gmail.com>
2015-03-17 12:22:23 -0400

    style of AsciiView updated somewhat

--- src/AsciiView.cc
+++ src/AsciiView.cc
@@ -1,62 +1,65 @@
 #include "../include/AsciiView.h"
 
-AsciiView::AsciiView(Game* game){
-     AsciiView::game = game;
+std::string visualization_header = std::string(
+  "WH```P`P`P`P`P`P``|BB|``P`P`P`P`P`P```P`P`P`P`P`P``|WB|``P`P`P`P`P`P```BH\n");
+
+AsciiView::AsciiView(Game* game) {
+  AsciiView::game = game;
 }
 
-char AsciiView::whiteStone(){
+char AsciiView::whiteStone() {
   incrementWhiteStones();
   return _whiteStone;
 }
 
-void AsciiView::incrementWhiteStones(){
+void AsciiView::incrementWhiteStones() {
   numWhiteStones = numWhiteStones + 1;
 }
 
-int AsciiView::whiteStoneCount(){
+int AsciiView::whiteStoneCount() {
   return numWhiteStones;
 }
 
-char AsciiView::blackStone(){
+char AsciiView::blackStone() {
   incrementBlackStones();
-	return _blackStone;
+  return _blackStone;
 }
 
-void AsciiView::incrementBlackStones(){
+void AsciiView::incrementBlackStones() {
   numBlackStones = numBlackStones + 1;
 }
 
-int AsciiView::blackStoneCount(){
+int AsciiView::blackStoneCount() {
   return numBlackStones;
 }
 
-std::string AsciiView::toString(){
-  //needs to print the turn number? maybe
-  //needs to print whose turn it is white or black
-  //Dice rolls
-  std::string visualization = std::string("WH```P`P`P`P`P`P``|BB|``P`P`P`P`P`P```P`P`P`P`P`P``|WB|``P`P`P`P`P`P```BH\n");
+std::string AsciiView::toString() {
+  // needs to print the turn number? maybe
+  // needs to print whose turn it is white or black
+  // Dice rolls
+  std::string visualization = visualization_header;
   std::vector<int> viewColumnArray;
-  viewColumnArray = getViewColumnArray(); 
-  
-  for(int i = 0; i < 15; i = i + 1){
-    for(int j = 1; j < 73; j = j + 1){
-        if(viewColumnArray[j] == 0 || abs(viewColumnArray[j]) <= i  ){
-          visualization.append(" ");
-        }else{
-           if(viewColumnArray[j] > 0){
-              visualization.append(charToString(whiteStone()));
-           }else{
-               visualization.append(charToString(blackStone()));
-           }
-       }
+  viewColumnArray = getViewColumnArray();
+  for (int i = 0; i < 15; i = i + 1) {
+    for (int j = 1; j < 73; j = j + 1) {
+      if (viewColumnArray[j] == 0 || abs(viewColumnArray[j]) <= i) {
+        visualization.append(" ");
+      }
+      else{
+        if (viewColumnArray[j] > 0) {
+          visualization.append(charToString(whiteStone()));
+        }
+        else{
+          visualization.append(charToString(blackStone()));
+        }
+      }
     }
-    visualization.append("\n");
+  visualization.append("\n");
   }
-
   return visualization;
 }
 
-std::vector<int> AsciiView::getViewColumnArray(){
+std::vector<int> AsciiView::getViewColumnArray() {
   Board* b = game->getBoard();
   std::vector<int> viewColumnArray(73,0);
   viewColumnArray[1] = b->homes[0];
@@ -67,7 +70,7 @@ std::vector<int> AsciiView::getViewColumnArray(){
   viewColumnArray[14] = b->pips[5];
   viewColumnArray[16] = b->pips[6];
   viewColumnArray[20] = b->bars[1];
-  viewColumnArray[25] = b->pips[7]; 
+  viewColumnArray[25] = b->pips[7];
   viewColumnArray[27] = b->pips[8];
   viewColumnArray[29] = b->pips[9];
   viewColumnArray[31] = b->pips[10];
@@ -87,14 +90,13 @@ std::vector<int> AsciiView::getViewColumnArray(){
   viewColumnArray[66] = b->pips[23];
   viewColumnArray[68] = b->pips[24];
   viewColumnArray[72] = b->homes[1];
-     
   return viewColumnArray;
 }
 
-std::string AsciiView::charToString(char c){
-   std::stringstream ss;
-   std::string s;
-   ss << c;
-   ss >> s;
+std::string AsciiView::charToString(char c) {
+  std::stringstream ss;
+  std::string s;
+  ss << c;
+  ss >> s;
   return s;
 }
